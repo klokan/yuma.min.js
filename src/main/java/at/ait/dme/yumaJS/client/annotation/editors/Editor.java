84d5be9dc9f1fa6e26c23b9772543119da1f8e7b
Rainer Simon <rainer.simon@ait.ac.at>
2011-09-21 17:23:27 +0200

    Cleanup

--- src/main/java/at/ait/dme/yumaJS/client/annotation/editors/Editor.java
+++ src/main/java/at/ait/dme/yumaJS/client/annotation/editors/Editor.java
@@ -1,20 +1,57 @@
 package at.ait.dme.yumaJS.client.annotation.editors;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+
 import at.ait.dme.yumaJS.client.annotation.core.Annotatable;
 import at.ait.dme.yumaJS.client.annotation.core.Annotation;
+import at.ait.dme.yumaJS.client.annotation.editors.selection.Selection;
+import at.ait.dme.yumaJS.client.annotation.widgets.EditForm;
 
 public abstract class Editor {
 	
-	private Annotatable annotatable; 
+	private Annotatable annotatable;
+	
+	protected Selection selection;
+	
+	protected EditForm editForm;
 	
 	public Editor(Annotatable annotatable) {
 		this.annotatable = annotatable;
+	}
+	
+	protected void setSelection(Selection selection) {
+		this.selection = selection;
+	}
+	
+	protected void setEditForm(EditForm editForm) {
+		this.editForm = editForm;
+		
+		this.editForm.addSaveClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				saveAnnotation();
+				destroy();
+			}
+		});
+		
+		this.editForm.addCancelClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				destroy();
+			}
+		});
 	}
 	
 	protected void saveAnnotation() {
 		annotatable.addAnnotation(getAnnotation());
 	}
 	
-	public abstract Annotation getAnnotation();
+	private Annotation getAnnotation() {
+		return Annotation.create(selection.getSelectedFragment(), editForm.getText());
+	}
+	
+	private void destroy() {
+		editForm.removeFromParent();
+		selection.destroy();
+	}
 	
 }
